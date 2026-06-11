c4e56770a11059d157439e4dc65a5921b7c5408e
Michael Robertson <michael@robertson-family.net>
2026-06-11 18:25:59 +1000

    feat: add reverse url search

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Depends, status
+from fastapi import FastAPI, Depends, status, HTTPException
 from app.database import create_db_tables, get_db
 from app import crud, schemas
 from sqlalchemy.orm import Session
@@ -29,3 +29,14 @@ def health_check():
 @app.post("/shorten", response_model=schemas.URLResponse, status_code=status.HTTP_201_CREATED)
 def create_short_url(url_data: schemas.URLCreate, db: Session = Depends(get_db)):
     return crud.create_short_url(db, url_data)
+
+# Read original url for short_code
+@app.get("/shorten/{short_code}", response_model=schemas.URLResponse)
+def get_short_url(short_code: str, db: Session = Depends(get_db)):
+    db_url = crud.get_url_by_short_code(db, short_code)
+
+    if db_url is None:
+        raise HTTPException(status_code=404, detail="Short URL not found")
+    
+    return db_url
+    
